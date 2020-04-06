40f0717edac5251433146bd55f9a73cbedb87a09
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2020-04-07 04:06:47 +0600

    Move element

---
+++ DS and Algorithm/AlgoExpert/Medium/moveElementToEnd.py
@@ -0,0 +1,13 @@
+def moveElementToEnd(array, toMove):
+    i = 0
+    j = len(array) - 1
+    while i < j:
+        while i < j and array[j] == toMove:
+            j -= 1
+        if array[i] == toMove:
+            array[i], array[j] = array[j], array[i]
+        i += 1
+    return array
+
+
+print(moveElementToEnd([1, 4, 2, 3, 2, 4, 2, 2, 2], 2))
