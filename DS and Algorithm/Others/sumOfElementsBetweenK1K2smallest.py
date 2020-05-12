0df9e3476ec8626ebd1deb8bc06d5c56817c38b0
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2020-05-12 16:02:09 +0600

    sum between k1 and k2

---
+++ DS and Algorithm/Others/sumOfElementsBetweenK1K2smallest.py
@@ -0,0 +1,14 @@
+import heapq
+
+
+def sumOfElementsBetweenK1K2smallest(arr, k1, k2):
+    first = heapq.nsmallest(k1, arr)
+    second = heapq.nsmallest(k2, arr)
+    sum = 0
+    for value in arr:
+        if value > first[-1] and value < second[-1]:
+            sum = sum + value
+    print(sum)
+
+
+print(sumOfElementsBetweenK1K2smallest([1, 2, 12, 5, 15, 11], 3, 6))
